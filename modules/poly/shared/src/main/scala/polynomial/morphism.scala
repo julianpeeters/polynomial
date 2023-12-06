7d6d47e35dd26a1f8a9da41dc22ac459432b5d62
Julian Peeters <julianpeeters@gmail.com>
2023-12-05 17:24:47 -0800

    Refactor

--- modules/poly/shared/src/main/scala/polynomial/morphism.scala
+++ modules/poly/shared/src/main/scala/polynomial/morphism.scala
@@ -1,41 +1,41 @@
 package polynomial
 
-import polynomial.functor.{Dir, Pos}
-import polynomial.`object`.{Monomial, Store}
+import polynomial.`object`.{Binomial, Monomial, Store}
  
 object morphism:
 
   type ~>[P[_], Q[_]] = PolyMap[P, Q, _]
 
   abstract class PolyMap[P[_], Q[_], Y]:
-    def φ: PolyMap.Phi[Pos[P, Y], Pos[Q, Y]]
-    def `φ#`: PolyMap.PhiSharp[Pos[P, Y], Dir[Q, Y], Dir[P, Y]]
+    def φ: PolyMap.Phi[P, Q, Y]
+    def `φ#`: PolyMap.PhiSharp[P, Q, Y]
 
   object PolyMap:
 
     def apply[P[_], Q[_], Y](
-      phi: PolyMap.Phi[Pos[P, Y], Pos[Q, Y]],
-      phiSharp: PolyMap.PhiSharp[Pos[P, Y], Dir[Q, Y], Dir[P, Y]]
+      phi: PolyMap.Phi[P, Q, Y],
+      phiSharp: PolyMap.PhiSharp[P, Q, Y]
     ): PolyMap[P, Q, Y] =
       new PolyMap[P, Q, Y]:
-        def φ: Phi[Pos[P, Y], Pos[Q, Y]] = phi
-        def `φ#`: PhiSharp[Pos[P, Y], Dir[Q, Y], Dir[P, Y]] = phiSharp
+        def φ: Phi[P, Q, Y] = phi
+        def `φ#`: PhiSharp[P, Q, Y] = phiSharp
     
-    extension [S, A, B, Y] (p: PolyMap[Store[S, _], Monomial[A, B, _], Y])
-      def andThen[Q[_]](w: PolyMap[Monomial[A, B, _], Q, Y]): PolyMap[PolyMap[Store[S, _], Monomial[A, B, _], _], Q, Y] =
-        new PolyMap[PolyMap[Store[S, _], Monomial[A, B, _], _], Q, Y]:
-          def φ: Phi[Pos[PolyMap[Store[S, _], Monomial[A, B, _], _], Y], Pos[Q, Y]] =
+    extension [S, A1, B1, Y] (p: PolyMap[Store[S, _], Monomial[A1, B1, _], Y])
+      def andThen[A2, B2](w: PolyMap[Monomial[A1, B1, _], Monomial[A2, B2, _], Y]): PolyMap[PolyMap[Store[S, _], Monomial[A1, B1, _], _], Monomial[A2, B2, _], Y] =
+        new PolyMap[PolyMap[Store[S, _], Monomial[A1, B1, _], _], Monomial[A2, B2, _], Y]:
+          def φ: PolyMap.Phi[PolyMap[Store[S, _], Monomial[A1, B1, _], _], Monomial[A2, B2, _], Y] =
             p.φ.andThen(w.φ)
-          def `φ#`: PhiSharp[Pos[PolyMap[Store[S, _], Monomial[A, B, _], _], Y], Dir[Q, Y], Dir[PolyMap[Store[S, _], Monomial[A, B, _], _], Y]] =
+          def `φ#`: PhiSharp[PolyMap[Store[S, _], Monomial[A1, B1, _], _], Monomial[A2, B2, _], Y] =
             (s, a) => p.`φ#`(s, w.`φ#`(p.φ(s), a))
 
-    type Phi[PosPs, PosQs] = (PosPs, PosQs) match
-      case (s, b)               => s => b
-      case ((s1, s2), (b1, b2)) => ((s1, s2) => b1, (s1, s2) => b2)
-      case ((s1, s2), b)        => (s1, s2) => b
-      case (s, (b1, b2))        => (s => b1, s => b2)
-      case (s, a => b)          => s => a => b
+    type Phi[P[_], Q[_], Y] = (P[Y], Q[Y]) match
+      case (PolyMap[Store[s, _], Monomial[a1, b1, _], Y], Monomial[a2, b2, Y]) => s => b2
+      case (Monomial[a1, b1, Y], Monomial[a2, b2, Y])                          => b1 => b2
+      case (Store[s, Y], Monomial[a, b, Y])                                    => s => b
+      case (Store[s, Y], Binomial[a1, b1, a2, b2, Y])                          => (s => b1, s => b2)
 
-    type PhiSharp[PosP, DirQ, DirP] = (PosP, DirQ, DirP) match
-      case (s1, a, s2)        => (s1, a) => s2
-      case (s1, (a1, a2), s2) => ((s1, a1) => s2, (s1, a2) => s2)
+    type PhiSharp[P[_], Q[_], Y] = (P[Y], Q[Y]) match
+      case (PolyMap[Store[s, _], Monomial[a1, b1, _], Y], Monomial[a2, b2, Y]) => (s, a2) => s
+      case (Monomial[a1, b1, Y], Monomial[a2, b2, Y])                          => (b1, a2) => a1
+      case (Store[s, Y], Monomial[a, b, Y])                                    => (s, a) => s
+      case (Store[s, Y], Binomial[a1, b1, a2, b2, Y])                          => ((s, a1) => s, (s, a2) => s)
