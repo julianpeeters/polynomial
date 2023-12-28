6089031e23fdb5a91524b2bc22a30e60d5ac3246
Julian Peeters <julianpeeters@gmail.com>
2023-12-27 16:04:07 -0800

    Add aliases for initial and terminal objects

--- modules/poly/shared/src/main/scala/polynomial/object.scala
+++ modules/poly/shared/src/main/scala/polynomial/object.scala
@@ -2,15 +2,25 @@ package polynomial
 
 object `object`:
 
+  // 0
+  type Intial[Y] = Monomial[Nothing, Nothing, Y]
+
+  // 1
+  type Terminal[Y] = Monomial[Nothing, Unit, Y]
+
+  // `Syˢ`
   type Store[S, Y] = cats.data.Store[S, Y]
 
+  // Byᴬ
   case class Monomial[A, B, Y](yᵉˣᵖ: A => Y, coeff: B)
 
+  // B₁yᴬ¹ + B₂yᴬ²
   sealed trait Binomial[+A1, +B1, +A2, +B2, Y]
   object Binomial:
     case class Term1[A1, B1, Y](yᵉˣᵖ: A1 => Y, coeff: B1) extends Binomial[A1, B1, Nothing, Nothing, Y]
     case class Term2[A2, B2, Y](yᵉˣᵖ: A2 => Y, coeff: B2) extends Binomial[Nothing, Nothing, A2, B2, Y]
 
+  // B₁yᴬ¹ + B₂yᴬ² + B₃yᴬ³
   sealed trait Trinomial[+A1, +B1, +A2, +B2, +A3, +B3, Y]
   object Trinomial:
     case class Term1[A1, B1, Y](yᵉˣᵖ: A1 => Y, coeff: B1) extends Trinomial[A1, B1, Nothing, Nothing, Nothing, Nothing, Y]
