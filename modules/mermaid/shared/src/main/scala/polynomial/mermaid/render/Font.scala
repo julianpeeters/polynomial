0b26076f362b4a396beb980cae25415e2da33fb6
Julian Peeters <julianpeeters@gmail.com>
2024-01-10 18:00:28 -0800

    Update README for github mermaid rendering

--- modules/mermaid/shared/src/main/scala/polynomial/mermaid/render/Font.scala
+++ modules/mermaid/shared/src/main/scala/polynomial/mermaid/render/Font.scala
@@ -3,7 +3,9 @@ package polynomial.mermaid.render
 object Font:
 
   def courier(text: String): String =
-    s"""<span style="font-family:Courier">$text</span>"""
+    // s"""<span style="font-family:Courier">$text</span>"""
+    s"""$text"""
 
   def superscript(text: String): String =
-    s"<sup>$text</sup>"
+    // s"<sup>$text</sup>"
+    s"^$text"
